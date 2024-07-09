19ed986edd90ce01c19976b269976f8abe7c6524
khanh <hoangkhanhvn179@gmail.com>
2024-07-09 21:46:22 +0700

    fix category export fe

--- front_end/src/pages/category/index.ts
+++ front_end/src/pages/category/index.ts
@@ -1,1 +1,5 @@
-export { default } from './CategoryManage'
+import CategoryCreate from './CategoryCreate'
+import CategoryManage from './CategoryManage'
+import CategoryUpdate from './CategoryUpdate'
+
+export { CategoryManage as default, CategoryCreate, CategoryUpdate }
