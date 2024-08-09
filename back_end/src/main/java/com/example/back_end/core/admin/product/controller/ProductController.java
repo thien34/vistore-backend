ebe7ed792ddad1d55f8b186b079c2dac5c1c5959
Haohero37 <haotbph32696@fpt.edu.vn>
2024-08-09 20:01:35 +0700

    Update URL ProductController

--- back_end/src/main/java/com/example/back_end/core/admin/product/controller/ProductController.java
+++ back_end/src/main/java/com/example/back_end/core/admin/product/controller/ProductController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/product")
+@RequestMapping("/admin/products")
 @Slf4j
 public class ProductController {
     private final ProductService productService;
