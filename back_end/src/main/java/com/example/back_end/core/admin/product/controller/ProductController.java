b9e2372229f026a42dc937e4bf864a5952e88451
Haohero37 <haotbph32696@fpt.edu.vn>
2024-08-09 19:59:15 +0700

    API ListName Product

---
+++ back_end/src/main/java/com/example/back_end/core/admin/product/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.example.back_end.core.admin.product.controller;
+
+import com.example.back_end.core.admin.manufacturer.payload.response.ManufacturerNameResponse;
+import com.example.back_end.core.admin.product.payload.response.ProductNameResponse;
+import com.example.back_end.core.admin.product.service.ProductService;
+import com.example.back_end.core.common.ResponseData;
+import com.example.back_end.core.common.ResponseError;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/product")
+@Slf4j
+public class ProductController {
+    private final ProductService productService;
+    @GetMapping("/listname")
+    public ResponseData<?> getAllManufacturersName() {
+        try {
+            List<ProductNameResponse> response = productService.getAllProductsName();
+            return new ResponseData<>(HttpStatus.OK.value(), "Get all Products name successfully", response);
+        } catch (Exception e) {
+            log.error("Error getting all Products name", e);
+            return new ResponseError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        }
+    }
+}
---
+++ back_end/src/main/java/com/example/back_end/core/admin/product/payload/response/ProductNameResponse.java
@@ -0,0 +1,14 @@
+package com.example.back_end.core.admin.product.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ProductNameResponse {
+    private Long id;
+    private String name;
+}
