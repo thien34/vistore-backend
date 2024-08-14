86ebd33918558e8b0b69c7c169b9293e218f72a4
nguyenkien <nguyenkienynk@gmail.com>
2024-08-15 02:13:43 +0700

    delete success-code and handle error-code

--- back_end/src/main/java/com/example/back_end/core/common/ResponseError.java
+++
@@ -1,8 +0,0 @@
-package com.example.back_end.core.common;
-
-public class ResponseError extends ResponseData {
-
-    public ResponseError(int status, String message) {
-        super(status, message);
-    }
-}
