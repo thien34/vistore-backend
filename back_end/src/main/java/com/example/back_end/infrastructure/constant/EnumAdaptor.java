93e86b6b6a00bbc3fa7012598e50f2f06550e45e
namnd <namnd@newwave.com.vn>
2024-09-15 10:13:50 +0700

    add common enum handling methods

---
+++ back_end/src/main/java/com/example/back_end/infrastructure/constant/EnumAdaptor.java
@@ -0,0 +1,66 @@
+package com.example.back_end.infrastructure.constant;
+
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Optional;
+
+@NoArgsConstructor
+public final class EnumAdaptor {
+
+
+    @SuppressWarnings("unchecked")
+    public static <E> E valueOf(int constantValue, Class<E> enumClass) {
+        Field valueField = getValueField(enumClass);
+
+        E[] constants = enumClass.getEnumConstants();
+
+        for (E constant : constants) {
+            int current = getValue(valueField, constant);
+            if (constantValue == current) {
+                return constant;
+            }
+        }
+
+        throw new IllegalArgumentException(String.format("not found: %d of %s", constantValue, enumClass.getName()));
+    }
+
+    public static <E> Optional<E> optionalOf(Integer constantValueNullable, Class<E> enumClass) {
+        return constantValueNullable == null ? Optional.empty() : Optional.of(valueOf(constantValueNullable, enumClass));
+    }
+
+    public static <E> Optional<E> optionalOf(Integer constantValueNullable, Class<E> enumClass, int emptyValue) {
+        return constantValueNullable == emptyValue ? Optional.empty() : optionalOf(constantValueNullable, enumClass);
+    }
+
+    public static <E extends Enum<?>> List<EnumConstant> convertToValueNameList(Class<E> enumClass, Enum[] enumConstants) {
+        return convertToValueNameList(enumClass, enumClass.getEnumConstants());
+    }
+
+
+    private static <E> Field getValueField(Class<E> enumClass) {
+        try {
+            return enumClass.getField("value");
+        } catch (SecurityException | NoSuchFieldException var2) {
+            throw new IllegalArgumentException(String.format("value field is not defined: %s", enumClass.getName()), var2);
+        }
+    }
+
+    private static <E> Field getNameIdField(Class<E> enumClass) {
+        try {
+            return enumClass.getField("nameId");
+        } catch (SecurityException | NoSuchFieldException var2) {
+            return null;
+        }
+    }
+
+    private static <E> int getValue(Field valueField, E constant) {
+        try {
+            return (int) (Integer) valueField.get(constant);
+        } catch (IllegalAccessException | IllegalArgumentException var4) {
+            throw new RuntimeException(var4);
+        }
+    }
+
+}
---
+++ back_end/src/main/java/com/example/back_end/infrastructure/constant/EnumConstant.java
@@ -0,0 +1,55 @@
+package com.example.back_end.infrastructure.constant;
+
+import java.beans.ConstructorProperties;
+
+public final class EnumConstant {
+    private final int value;
+    private final String fieldName;
+
+    @ConstructorProperties({"value", "fieldName"})
+    public EnumConstant(int value, String fieldName) {
+        this.value = value;
+        this.fieldName = fieldName;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String getFieldName() {
+        return this.fieldName;
+    }
+
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof EnumConstant)) {
+            return false;
+        } else {
+            EnumConstant other = (EnumConstant) o;
+            if (this.getValue() != other.getValue()) {
+                return false;
+            } else {
+                Object this$fieldName = this.getFieldName();
+                Object other$fieldName = other.getFieldName();
+                if (this$fieldName == null) {
+                    return other$fieldName == null;
+                } else return this$fieldName.equals(other$fieldName);
+            }
+        }
+    }
+
+    public int hashCode() {
+        int result = 1;
+        result = result * 59 + this.getValue();
+        Object $fieldName = this.getFieldName();
+        result = result * 59 + ($fieldName == null ? 43 : $fieldName.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "EnumConstant(value=" + this.getValue() + ", fieldName=" + this.getFieldName() + ", localizedName=" +  ")";
+    }
+}
+
