38478f3df7acc76e628baf4ef7f51e827ef90675
nguyenkien <nguyenkienynk@gmail.com>
2024-09-18 14:57:16 +0700

    #33 - pushing enum

---
+++ front_end/src/pages/discount/DiscountLimitationEnum.ts
@@ -0,0 +1,10 @@
+export enum DiscountLimitationEnum {
+    UNLIMITED = '0',
+    N_TIMES_ONLY = '1',
+    N_TIMES_PER_CUSTOMER = '2',
+}
+export enum DiscountLimitationNumberEnum {
+    UNLIMITED = 0,
+    N_TIMES_ONLY = 1,
+    N_TIMES_PER_CUSTOMER = 2,
+}
