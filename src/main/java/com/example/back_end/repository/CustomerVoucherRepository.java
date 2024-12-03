0333aef9f8f29db875beabd6c1889567d72ac926
nguyenkien <nguyenkienynk@gmail.com>
2024-12-03 20:49:35 +0700

    #47 - feat/voucher: fix update voucher

--- src/main/java/com/example/back_end/repository/CustomerVoucherRepository.java
+++ src/main/java/com/example/back_end/repository/CustomerVoucherRepository.java
@@ -5,10 +5,12 @@ import com.example.back_end.entity.CustomerVoucher;
 import com.example.back_end.entity.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerVoucherRepository extends JpaRepository<CustomerVoucher, Long> {
     Optional<CustomerVoucher> findByCustomerIdAndDiscountId(Long customerId, Long discountId);
     boolean existsByCustomerAndDiscount(Customer customer, Discount discount);
+    List<CustomerVoucher> findByDiscount(Discount discount);
 
 }
