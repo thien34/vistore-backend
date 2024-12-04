69d22d6a3d63b6fba88013bcff46effc1c64147e
Haohero37 <haotbph32696@fpt.edu.vn>
2024-12-04 16:58:20 +0700

    Add more OrderFilter(startAmountTotal,endAmountTotal,status,paymentStatus)

--- src/main/java/com/example/back_end/core/admin/order/payload/OrderFilter.java
+++ src/main/java/com/example/back_end/core/admin/order/payload/OrderFilter.java
@@ -1,10 +1,13 @@
 package com.example.back_end.core.admin.order.payload;
 
+import com.example.back_end.infrastructure.constant.OrderStatusType;
+import com.example.back_end.infrastructure.constant.PaymentStatusType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 
 @Data
@@ -14,6 +17,10 @@ public class OrderFilter {
 
     private String name;
     private Integer paymentMode;
+    private BigDecimal startAmount;
+    private BigDecimal endAmount;
+    private OrderStatusType status;
+    private PaymentStatusType paymentStatus;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private Instant startDate;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
--- src/main/java/com/example/back_end/service/order/impl/OrderSpecification.java
+++ src/main/java/com/example/back_end/service/order/impl/OrderSpecification.java
@@ -2,32 +2,50 @@ package com.example.back_end.service.order.impl;
 
 import com.example.back_end.core.admin.order.payload.OrderFilter;
 import com.example.back_end.entity.Order;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class OrderSpecification {
     public static Specification<Order> filterBy(OrderFilter filter) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
-
-            if (filter.getName() != null && !filter.getName().isEmpty()) {
-                predicates.add(criteriaBuilder.like(
-                        criteriaBuilder.lower(root.get("customer").get("firstName")),
-                        "%" + filter.getName().toLowerCase() + "%"
-                ));
+            Optional.ofNullable(filter.getName())
+                    .map(String::toLowerCase).ifPresent(nameFilter -> {
+                        Path<String> firstNamePath = root.get("customer").get("firstName");
+                        Path<String> lastNamePath = root.get("customer").get("lastName");
+                        Expression<String> customerName = criteriaBuilder.concat(
+                                criteriaBuilder.concat(firstNamePath, " "),
+                                lastNamePath
+                        );
+                        predicates.add(criteriaBuilder.like(
+                                criteriaBuilder.lower(customerName),
+                                "%" + nameFilter + "%"
+                        ));
+                    });
+            if (filter.getStartAmount() != null) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("orderTotal"), filter.getStartAmount()));
             }
-
+            if (filter.getEndAmount() != null) {
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("orderTotal"), filter.getEndAmount()));
+            }
             if (filter.getPaymentMode() != null) {
                 predicates.add(criteriaBuilder.equal(root.get("paymentMethodId"), filter.getPaymentMode()));
             }
-
             if (filter.getStartDate() != null) {
                 predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("createdDate"), filter.getStartDate()));
             }
-
+            if (filter.getStatus() != null) {
+                predicates.add(criteriaBuilder.equal(root.get("orderStatusId"), filter.getStatus()));
+            }
+            if (filter.getPaymentStatus() != null) {
+                predicates.add(criteriaBuilder.equal(root.get("paymentStatusId"), filter.getPaymentStatus()));
+            }
             if (filter.getEndDate() != null) {
                 predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("createdDate"), filter.getEndDate()));
             }
